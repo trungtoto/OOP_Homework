4c9de1e3c75a734e599864630423a1c1a23f30d1
Lê Bá Thành Trung <141932678+trungtoto@users.noreply.github.com>
2024-11-14 22:17:07 +0700

    ok

---
+++ LoginSystem/src/LoginSystem/main.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package LoginSystem;
+
+/**
+ *
+ * @author ADMIN
+ */
+public class main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        NewUser myFrame = new NewUser();
+        myFrame.setSize(700, 400);
+        
+        myFrame.setVisible(true);
+    }
+    
+}
